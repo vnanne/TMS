b97dcf0fdd125c66510d4e39191b5d91022d170c
Suresh Chowdary <sureshbabuk782@gmail.com>
2019-10-21 22:13:04 +0530

    Update admin.py

--- boards/admin.py
+++ boards/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import Board
+from .models import Board,Customer,Workorder,Address,Address_Type,Receiver,Terminals,Import,Export
 
 admin.site.register(Board)
+admin.site.register(Customer)
+admin.site.register(Workorder)
+admin.site.register(Address)
+admin.site.register(Address_Type)
+admin.site.register(Receiver)
+admin.site.register(Terminals)
+admin.site.register(Receiver)
+admin.site.register(Import)
+admin.site.register(Export)
+
+
